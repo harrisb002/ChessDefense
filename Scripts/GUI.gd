4d505334c4ac24986e26739bb3a8cbf6d1324ab2
harrisb002 <harrisbe@sonoma.edu>
2024-09-08 01:28:36 -0700

    Piece movement/captures

--- Scripts/GUI.gd
+++ Scripts/GUI.gd
@@ -37,7 +37,7 @@ func _ready() -> void:
 		piece_array.fill(0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 # Creates a new slot (Square) for the grid and adds it to the grid_array
@@ -46,7 +46,34 @@ func create_slot():
 	new_slot.slot_ID = grid_array.size() # Set the slotID to the current gridSize
 	board_grid.add_child(new_slot) # Add the slot to the board grid
 	grid_array.push_back(new_slot) # Add a slot instance in the grid array/inc. size
+	
+	# Utilize the slot clicked signal by connecting the slot signal
+	new_slot.slot_clicked.connect(_on_slot_clicked)
 
+
+# Move the piece based on the slot clicked on (If a piece is selected)
+func _on_slot_clicked(slot) -> void:
+	if not piece_selected:
+		return # Dont do anything if piece not selected
+	move_piece(piece_selected, slot.slot_ID) # move a piece if selected 
+	piece_selected = null # After piece has been moved, then set it back to null
+
+
+# Move the selected piece to the slot clicked on
+func move_piece(piece, location) -> void:
+	# Capture Logic
+	if piece_array[location]: # If piece resides at the location
+		if piece_array[location] == piece:
+			return
+		piece_array[location].queue_free() # Remove the piece at the new location 
+		piece_array[location] = 0
+	
+	# Set the new location using tween
+	var tween = get_tree().create_tween()
+	tween.tween_property(piece, "global_position", grid_array[location].global_position + icon_offset, 0.2)
+	piece_array[piece.slot_ID] = 0 # delete the piece from its original position 
+	piece_array[location] = piece # Add it to the new location	
+	piece.slot_ID = location # Update the slotID
 
 func add_piece(piece_type, location) -> void:
 	var new_piece = piece_scene.instantiate() # Create a new piece instance
@@ -64,13 +91,16 @@ func add_piece(piece_type, location) -> void:
 	
 	# Add a connection using the name of the signal passed by the piece
 	new_piece.piece_selected.connect(_on_piece_selected)
-	
+
 
 func _on_piece_selected(piece):
 	if not piece_selected:
 		piece_selected = piece # Set it to the passed piece from the emitted signal
 	else:
-		pass
+		# I clicking on a piece that is not the piece currently selected, then direct it into the slot clicked
+		# This is for captures
+		_on_slot_clicked(grid_array[piece.slot_ID])
+		
 
 
 # Use a bitboard (0-63 bits) to map the color of the filter 
@@ -85,7 +115,8 @@ func set_board_filter(bitmap : int):
 func _on_test_button_pressed() -> void:
 	#add_piece(DataHandler.PieceNames.BLACK_KING, 3)
 	parse_fen(fen) # Create starting board
-	set_board_filter(1023)
+	#set_board_filter(1023)
+
 
 func parse_fen(fen : String) -> void:
 	var boardstate = fen.split(" ")
