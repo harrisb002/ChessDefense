57183c7612a4eda498c76e407c4e5b02d5c2a675
harrisb002 <harrisbe@sonoma.edu>
2024-09-07 20:29:48 -0700

    Creating grid on GUI scene

--- Scripts/GUI.gd
+++ Scripts/GUI.gd
@@ -1,5 +1,12 @@
 extends Control
 
+# Create References
+@onready var slot_scene = preload("res://Scenes/slot.tscn")
+@onready var board_grid = $ChessBoard/BoardGrid
+
+
+# Create container to save variables (Fetch slot instances through this)
+var grid_array := []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +16,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func create_slots():
+	# Instantiate new slot
+	var new_slot = slot_scene.instantiate()
+	new_slot.slot_ID = grid_array.size()
+	grid_array.push_back(new_slot)
---
+++ Scripts/slot.gd
@@ -0,0 +1,13 @@
+extends ColorRect
+
+# Create a Reference to the Slot
+var slot_ID := -1
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
