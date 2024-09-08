7c4f7057d9c3957b7f0bbcda886d10d455965227
harrisb002 <harrisbe@sonoma.edu>
2024-09-07 21:41:03 -0700

    Create board

--- Scripts/GUI.gd
+++ Scripts/GUI.gd
@@ -10,6 +10,18 @@ var grid_array := []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	for i in range(64):
+		create_slot()
+		
+	# Set color background based on even/odd
+	var colorbit := 0
+	for i in range(8):
+		for j in range(8):
+			if j%2 == colorbit:
+				grid_array[i*8+j].set_background(Color.CADET_BLUE)
+		if colorbit == 0: # Flip bit cause each starting row is dif.
+			colorbit = 1
+		else: colorbit = 0
 	pass # Replace with function body.
 
 
@@ -18,8 +30,9 @@ func _process(delta: float) -> void:
 	pass
 
 
-func create_slots():
+func create_slot():
 	# Instantiate new slot
 	var new_slot = slot_scene.instantiate()
 	new_slot.slot_ID = grid_array.size()
+	board_grid.add_child(new_slot)
 	grid_array.push_back(new_slot)
